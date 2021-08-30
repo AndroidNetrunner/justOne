9dd41495b72f14b90f0963a8f5def19043a14359
byukim <kby9906@naver.com>
2021-08-30 23:56:27 +0900

    [FIX] 힌트를 두 번 적을 경우 기존 힌트를 삭제하도록 수정

--- game_data.py
+++ game_data.py
@@ -1,23 +1,24 @@
 class game_data:
-	def __init__(self):
-		self.members = []
-		self.round = 13
-		self.hints = {}
-		self.words = open("word_list.txt", 'r', encoding='UTF-8').read().split('\n')
-		self.already = []
-		self.guesser = None
-		self.main_channel = None
-		self.guess = None
-		self.start = False
-		self.hint_time = False
-		self.starter = None
-		self.word = None
-		self.confirmed = None
-		self.checking = False
-		self.current_round = 0
-		self.hint_submission = 0
-		self.submitted_hints = ""
-
+    def __init__(self):
+        self.members = []
+        self.round = 13
+        self.hints = {}
+        self.words = open("word_list.txt", 'r', encoding='UTF-8').read().split('\n')
+        self.already = []
+        self.guesser = None
+        self.main_channel = None
+        self.guess = None
+        self.start = False
+        self.hint_time = False
+        self.starter = None
+        self.word = None
+        self.confirmed = None
+        self.checking = False
+        self.current_round = 0
+        self.hint_submission = 0
+        self.submitted_hints = ""
+        self.submitted = {}
+        
 # game_data = {
 # 	'members': [],
 # 	'round': 13,
