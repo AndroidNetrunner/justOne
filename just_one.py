5c744d62fe7bd2309cda04d65860589726b05a99
AndroidNetrunner <kby9906@naver.com>
2021-09-05 05:24:35 +0000

    [FIX] not sending check message

--- just_one.py
+++ just_one.py
@@ -102,7 +102,7 @@ async def on_message(message):
                 if current_game.hint_time:  # 힌트 제시
                     await submit_hint(current_game, message)
                     if current_game.hint_submission >= len(current_game.members) - 1: # 힌트 검수 시작
-                        start_checking_hints(current_game)
+                        await start_checking_hints(current_game)
                 else:  # 힌트 검수 중
                     confirmer = current_game.members[1] if current_game.starter == current_game.guesser else current_game.starter
                     if message.author == confirmer:
